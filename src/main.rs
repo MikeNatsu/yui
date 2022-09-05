245139a9044994c6efa3ce81a151e29f7bccc0b7
MikeNatsu <michaelgonzalezsantamaria@gmail.com>
2022-09-05 16:16:22 -0400

    test

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,14 @@ async fn main() {
         .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
         .group(&GENERAL_GROUP);
     let token = env::var("DISCORD_TOKEN").expect("CANNOT READ TOKEN");
+    println!("YUI is running {}", token.clone());
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
         .framework(framework)
         .await
         .expect("Error creating client");
+
 
     // start listening for events by starting a single shard
     if let Err(why) = client.start().await {
