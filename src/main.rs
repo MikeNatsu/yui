95a937e76a0d90fc4b88f99b201bfea12baaf39f
MikeNatsu <michaelgonzalezsantamaria@gmail.com>
2023-01-24 20:25:39 -0500

    feat: adding mcu api

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use serenity::prelude::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Tunnel { 
+struct Tunnel {
     id: String,
     public_url: String,
     proto: String,
@@ -24,8 +24,17 @@ struct TunnelsBody {
     uri: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct McuData {
+    days_until: i32,
+    title: String,
+    overview: String,
+    poster_url: String,
+    release_date: String,
+}
+
 #[group]
-#[commands(ping, clear, ngrok)]
+#[commands(ping, clear, ngrok, mcu)]
 struct General;
 
 struct Handler;
@@ -36,6 +45,7 @@ impl EventHandler for Handler {}
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+
     let framework = StandardFramework::new()
         .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
         .group(&GENERAL_GROUP);
@@ -61,15 +71,50 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
 }
 
 #[command]
+async fn mcu(ctx: &Context, msg: &Message) -> CommandResult {
+    let client = reqwest::Client::new();
+    let body = client
+        .get("https://www.whenisthenextmcufilm.com/api")
+        .send()
+        .await;
+    let data = body.unwrap().json::<McuData>().await.unwrap();
+    msg.channel_id
+        .send_message(ctx, |m| {
+            m.content("got you!!").tts(false).embed(|e| {
+                e.title(data.title).image(data.poster_url).field(
+                    "Release date",
+                    data.release_date,
+                    true,
+                )
+            })
+        })
+        .await
+        .unwrap();
+
+    Ok(())
+}
+
+#[command]
 async fn ngrok(ctx: &Context, msg: &Message) -> CommandResult {
     let ngrok_api = env::var("NGROK_API_TOKEN").expect("Something went wrong");
     let client = reqwest::Client::new();
-    let mut headers = HeaderMap::new(); 
-    headers.insert("Authorization", format!("Bearer {}", ngrok_api).parse().unwrap());
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        "Authorization",
+        format!("Bearer {}", ngrok_api).parse().unwrap(),
+    );
     headers.insert("Ngrok-Version", "2".to_string().parse().unwrap());
-    let body = client.get("https://api.ngrok.com/tunnels").headers(headers).send().await;
+    let body = client
+        .get("https://api.ngrok.com/tunnels")
+        .headers(headers)
+        .send()
+        .await;
     let data = body.unwrap().json::<TunnelsBody>().await.unwrap();
-    let urls: Vec<String> = data.tunnels.iter().map(|x| x.public_url.to_string()).collect();
+    let urls: Vec<String> = data
+        .tunnels
+        .iter()
+        .map(|x| x.public_url.to_string())
+        .collect();
     let message_value = format!("Tunnels:\n{} ", urls.join("\n"));
     msg.reply(ctx, message_value).await?;
 
